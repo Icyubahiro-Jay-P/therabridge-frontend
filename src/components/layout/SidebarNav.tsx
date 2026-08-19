db057b3b12aa56ee15d61448a88040b7eb8b2bc2
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-08-19 20:57:25 +0200

    feat: add Moon icon import to SidebarNav component

--- src/components/layout/SidebarNav.tsx
+++ src/components/layout/SidebarNav.tsx
@@ -1,6 +1,6 @@
 import {
   Home, LayoutDashboard, MessageCircle, Users, Heart,
-  Stethoscope, User, Shield, ShieldCheck, AlertTriangle, Bell, Settings, Brain, ClipboardCheck, Sparkles, Calendar, Route, BookOpen,
+  Stethoscope, User, Shield, ShieldCheck, AlertTriangle, Bell, Settings, Brain, ClipboardCheck, Sparkles, Calendar, Route, BookOpen, Moon,
 } from "lucide-react"
 import { NavItem, NavItemWithBadge } from "./NavItem"
 

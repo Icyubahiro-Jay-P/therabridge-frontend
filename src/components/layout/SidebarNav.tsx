ee049690e18ccab81315b7410a7dd1335cc9a19b
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-08-19 21:06:09 +0200

    feat: add Companion link to SidebarNav for pet management

--- src/components/layout/SidebarNav.tsx
+++ src/components/layout/SidebarNav.tsx
@@ -1,6 +1,6 @@
 import {
   Home, LayoutDashboard, MessageCircle, Users, Heart,
-  Stethoscope, User, Shield, ShieldCheck, AlertTriangle, Bell, Settings, Brain, ClipboardCheck, Sparkles, Calendar, Route, BookOpen, Moon, Pill,
+  Stethoscope, User, Shield, ShieldCheck, AlertTriangle, Bell, Settings, Brain, ClipboardCheck, Sparkles, Calendar, Route, BookOpen, Moon, Pill, Bird,
 } from "lucide-react"
 import { NavItem, NavItemWithBadge } from "./NavItem"
 
@@ -31,6 +31,7 @@ export function SidebarNav({
           <NavItem to="/learn" icon={<BookOpen className="size-4" />} label="Learn" minimized={isMinimized} onClick={closeMobile} />
           <NavItem to="/sleep" icon={<Moon className="size-4" />} label="Sleep" minimized={isMinimized} onClick={closeMobile} />
           <NavItem to="/medications" icon={<Pill className="size-4" />} label="Medications" minimized={isMinimized} onClick={closeMobile} />
+          <NavItem to="/pet" icon={<Bird className="size-4" />} label="Companion" minimized={isMinimized} onClick={closeMobile} />
           <NavItem to="/programs" icon={<Route className="size-4" />} label="Programs" minimized={isMinimized} onClick={closeMobile} />
           <NavItem to="/coping-cards" icon={<Layers className="size-4" />} label="Coping Cards" minimized={isMinimized} onClick={closeMobile} />
           <NavItem to="/therapists" icon={<Stethoscope className="size-4" />} label="Therapists" minimized={isMinimized} onClick={closeMobile} />

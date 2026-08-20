bbd1ca70312d7b33db39931159ba83edabbd3627
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-08-20 16:33:56 +0200

    fix: replace handleDateParts with setDateOfBirth in SignupPage for consistency

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -19,7 +19,7 @@ export function SignupPage() {
     isFirstStep,
     updateField,
     handleBlur,
-    handleDateParts,
+    setDateOfBirth,
     handleStepSubmit,
     setShowPassword,
     goToStep,
@@ -73,7 +73,7 @@ export function SignupPage() {
             isFirstStep={isFirstStep}
             updateField={updateField}
             handleBlur={handleBlur}
-            handleDateParts={handleDateParts}
+            setDateOfBirth={setDateOfBirth}
             handleStepSubmit={handleStepSubmit}
             setShowPassword={setShowPassword}
             goToStep={goToStep}

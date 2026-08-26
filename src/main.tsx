f9b569bb4d635a413dfbf34d1fbd83a9c19cbadd
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-08-26 08:47:55 +0200

    refactor: move BrowserRouter to wrap TooltipProvider for improved routing structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { Toaster } from "sonner"
 import { Analytics } from "@vercel/analytics/react"
@@ -19,12 +20,14 @@ createRoot(document.getElementById("root")!).render(
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-          <TooltipProvider>
-            <OnboardingProvider>
-              <App />
-            </OnboardingProvider>
-            <Toaster position="bottom-right" closeButton />
-          </TooltipProvider>
+          <BrowserRouter>
+            <TooltipProvider>
+              <OnboardingProvider>
+                <App />
+              </OnboardingProvider>
+              <Toaster position="bottom-right" closeButton />
+            </TooltipProvider>
+          </BrowserRouter>
         </ThemeProvider>
       </QueryClientProvider>
     </ErrorBoundary>

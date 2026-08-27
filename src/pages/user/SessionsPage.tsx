fb5459e5701fdc81320e351b94553fc580add216
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-08-27 18:41:33 +0200

    refactor: remove unused useEffect import in SessionsPage

--- src/pages/user/SessionsPage.tsx
+++ src/pages/user/SessionsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react"
+import { useMemo, useState } from "react"
 import { useNavigate, useSearchParams } from "react-router-dom"
 import {
   CalendarDays,
